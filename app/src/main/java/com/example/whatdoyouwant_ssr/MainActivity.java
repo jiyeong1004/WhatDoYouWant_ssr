cb03996a2254ede368d1e2045783f3e749aaef9a
jiyeong1004 <ak1157@naver.com>
2020-11-07 20:26:04 +0900

    Start button and screen switching

--- app/src/main/java/com/example/whatdoyouwant_ssr/MainActivity.java
+++ app/src/main/java/com/example/whatdoyouwant_ssr/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.whatdoyouwant_ssr;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton btn_start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_start = findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startedintent = new Intent(MainActivity.this, StartedActivity.class);
+                MainActivity.this.startActivity(startedintent);
+            }
+        });
+
     }
 }
