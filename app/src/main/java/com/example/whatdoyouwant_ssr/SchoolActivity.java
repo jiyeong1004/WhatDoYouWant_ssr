170ebdeb39e6fe7374a75ec7c800542837ddd99b
jiyeong1004 <ak1157@naver.com>
2020-11-12 19:07:03 +0900

    Add

---
+++ app/src/main/java/com/example/whatdoyouwant_ssr/SchoolActivity.java
@@ -0,0 +1,14 @@
+package com.example.whatdoyouwant_ssr;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SchoolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school);
+    }
+}
