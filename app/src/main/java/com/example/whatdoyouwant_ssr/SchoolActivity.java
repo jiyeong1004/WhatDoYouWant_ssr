9372a4f9c5dff7dd02c9a76683b7e7340546a43a
jiyeong1004 <ak1157@naver.com>
2020-11-16 22:37:40 +0900

    link ok

--- app/src/main/java/com/example/whatdoyouwant_ssr/SchoolActivity.java
+++ app/src/main/java/com/example/whatdoyouwant_ssr/SchoolActivity.java
@@ -1,6 +1,9 @@
 package com.example.whatdoyouwant_ssr;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,18 @@ public class SchoolActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school);
+
+        findViewById(R.id.school_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicked_btn("https://map.naver.com/v5/search/%EC%8B%9C%EC%9E%A5%EB%B6%84%EC%8B%9D/place/1344551600?c=14128799.4575170,4506643.5846943,15,0,0,0,dh&placePath=%3F%2526");
+            }
+        });
+    }
+    public void clicked_btn(String url)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 }
